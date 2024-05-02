2cf8c7641e58e04c8d415e0b2abbe0d936a0a537
Kevin Comba <kelchospense88@gmail.com>
2024-05-02 14:21:40 +0300

    deploy

--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -1,22 +1,34 @@
 
 import "dotenv/config";
-import { drizzle } from "drizzle-orm/node-postgres";
-import { Client } from "pg";
 import * as schema from "./schema";  //import all schemas from the schema file
+// import { drizzle } from "drizzle-orm/node-postgres";
+// import { Client } from "pg";
 
-//get the database url from  env vars & create a new client
-export const client = new Client({
-  connectionString: process.env.DATABASE_URL as string, 
-});
+// //get the database url from  env vars & create a new client
+// export const client = new Client({
+//   connectionString: process.env.DATABASE_URL as string, 
+// });
 
-//connect to the database
-const main = async () => {
-  await client.connect(); 
-}
-main();
+// //connect to the database
+// const main = async () => {
+//   await client.connect(); 
+// }
+// main();
 
-//create a drizzle instance with the client and schema
-const db = drizzle(client, { schema, logger: true });  
+// //create a drizzle instance with the client and schema
+// const db = drizzle(client, { schema, logger: true });  
 
-//export the db instance to be used in other files
+// //export the db instance to be used in other files
+// export default db;
+
+
+
+// NEON
+
+import { neon } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
+
+export const connection = neon(process.env.DATABASE_URL!);
+const db = drizzle(connection,{ schema, logger: true });
+
 export default db;
